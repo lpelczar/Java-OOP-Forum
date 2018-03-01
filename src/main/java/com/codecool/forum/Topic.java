086f832b5730ec86a09e5ae61fd8361c0bc9aec9
lpelczar <lukepelczar@gmail.com>
2018-03-01 12:54:25 +0100

    Add list of comments to topic and implement adding new comments to topic

--- src/main/java/com/codecool/forum/Topic.java
+++ src/main/java/com/codecool/forum/Topic.java
@@ -1,12 +1,20 @@
 package main.java.com.codecool.forum;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Topic extends Entry {
 
     private String title;
+    private List<Comment> comments = new ArrayList<>();
 
     public Topic(String title, String message) {
         super();
         this.title = title;
         super.setMessage(message);
+    }
+
+    public boolean addComment(Comment comment) {
+        return comments.add(comment);
     }
 }
