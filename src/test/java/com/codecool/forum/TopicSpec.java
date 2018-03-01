269fa56e0be3850cb3f921a179fe063934d5318e
lpelczar <lukepelczar@gmail.com>
2018-03-01 13:29:24 +0100

    Add test checking getting moderated comments

--- src/test/java/com/codecool/forum/TopicSpec.java
+++ src/test/java/com/codecool/forum/TopicSpec.java
@@ -5,6 +5,10 @@ import main.java.com.codecool.forum.Topic;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TopicSpec {
@@ -27,5 +31,19 @@ public class TopicSpec {
         Comment comment = new Comment("Comment1");
         this.topic.addComment(comment);
         assertEquals(1, topic.getComments().size());
+    }
+
+    @Test
+    public void whenGetModeratedCommentsThenReturnCommentsWhichAreModerated() {
+        Comment comment1 = new Comment("Comment1");
+        Comment comment2 = new Comment("Comment2");
+        Comment comment3 = new Comment("Comment3");
+        comment1.toggleModeration();
+        comment3.toggleModeration();
+        List<Comment> moderatedComments = Arrays.asList(comment1, comment3);
+        topic.addComment(comment1);
+        topic.addComment(comment2);
+        topic.addComment(comment3);
+        assertArrayEquals(moderatedComments.toArray(), topic.getModeratedComments().toArray());
     }
 }
