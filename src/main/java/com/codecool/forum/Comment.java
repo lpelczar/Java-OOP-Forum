dc0f8f02df3bac5a33dd8f2924a2042feef8a22e
lpelczar <lukepelczar@gmail.com>
2018-03-01 12:59:40 +0100

    Add moderated flag to comment class

--- src/main/java/com/codecool/forum/Comment.java
+++ src/main/java/com/codecool/forum/Comment.java
@@ -2,7 +2,13 @@ package main.java.com.codecool.forum;
 
 public class Comment extends Entry {
 
+    private boolean moderated = false;
+
     public Comment(String message) {
         super.setMessage(message);
+    }
+
+    public void toggleModeration() {
+        this.moderated = true;
     }
 }
